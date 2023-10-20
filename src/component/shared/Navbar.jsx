c2340957bfe1be0fe04e14c92c21b7834dced4b5
mk-saadi <mksaadi820@gmail.com>
2023-10-20 16:17:30 +0600

    first commit

---
+++ src/component/shared/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+            <p>nav</p>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Admin from "./component/admin/Admin";
+import Home from "./component/home/Home";
+import Login from "./component/auth/Login";
+import Register from "./component/auth/Register";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Admin />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
