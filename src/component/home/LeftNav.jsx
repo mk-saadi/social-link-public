9470545f114944649e7659888aeef207809e6b69
naserwp <naserwp@gmail.com>
2023-10-20 20:29:38 +0600

    Left Nev done

--- src/component/home/LeftNav.jsx
+++ src/component/home/LeftNav.jsx
@@ -1,5 +1,44 @@
+
+// import { FaHome } from 'react-icons/fa';
 const LeftNav = () => {
-    return <div></div>;
+    return (
+        <div className="">
+            {/* bg-blue-500 p-4 text-white */}
+            
+            {/* <div className="mb-4 text-center">
+               
+                <h3></h3>
+            </div> */}
+
+         
+            {/* <div className="space-y-4">
+               
+                <div className="flex items-center space-x-2">
+                    <div className="w-6 h-6">
+                        <FaHome className=' className="w-6 h-6"'></FaHome>
+                       
+                    </div>
+                    <span className="text-lg">Home</span>
+                </div>
+
+               
+                <div className="flex items-center space-x-2">
+                    <div className="w-6 h-6">
+                    <FaHome className=' className="w-6 h-6"'></FaHome>
+                    </div>
+                    <span className="text-lg">Message</span>
+                </div>
+
+                
+                <div className="flex items-center space-x-2">
+                    <div className="w-6 h-6">
+                    <FaHome className=' className="w-6 h-6"'></FaHome>
+                    </div>
+                    <span className="text-lg">Notifications</span>
+                </div>
+            </div> */}
+        </div>
+    );
 };
 
 export default LeftNav;
