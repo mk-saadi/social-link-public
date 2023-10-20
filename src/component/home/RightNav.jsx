e9a46432760993240b67aba951fe2ace21c31872
mk-saadi <mksaadi820@gmail.com>
2023-10-20 18:32:37 +0600

    navs

---
+++ src/component/home/RightNav.jsx
@@ -0,0 +1,118 @@
+const RightNav = () => {
+    return (
+        <div>
+            <div className="flex justify-around items-center">
+                <div className="flex justify-center items-center mt-4">
+                    <div className="avatar">
+                        <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img
+                                src="https://img.freepik.com/free-psd/3d-illustration-person-with-glasses_23-2149436185.jpg?w=740&t=st=1697801361~exp=1697801961~hmac=b43ddf8378b474f815843d062fefcd9502f9324d23496dd67f91a16e6112b293"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="ml-4 ">
+                        <p className="text-gray-100 text-2xl">alissa_blair88</p>
+                        <p className="text-lg text-gray-400">Alissa Blair</p>
+                    </div>
+                </div>
+                <p className="text-sky-400 font-semibold text-xl">Switch</p>
+            </div>
+
+            <div className="mt-12">
+                <p className="text-2xl text-gray-200 ml-4">People you may know</p>
+
+                <div className="flex flex-col">
+                    <div className="flex justify-center items-center">
+                        <div className="col-span-3 flex justify-center items-center mt-4">
+                            <div className="avatar">
+                                <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                    <img
+                                        src="https://img.freepik.com/free-psd/3d-illustration-person-with-glasses_23-2149436191.jpg?size=626&ext=jpg&ga=GA1.1.448863966.1693684736&semt=sph"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <div className="ml-4 ">
+                                <p className="text-gray-100 text-2xl">susan_atkinson5</p>
+                                <p className="text-lg text-gray-400">Susan Atkinson</p>
+                            </div>
+                        </div>
+                        <p className="text-sky-400 font-semibold text-xl ">Follow</p>
+                    </div>
+
+                    <div className="grid grid-cols-4">
+                        <div className="col-span-3 flex justify-center items-center mt-4">
+                            <div className="avatar">
+                                <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                    <img
+                                        src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671122.jpg?size=626&ext=jpg&ga=GA1.1.448863966.1693684736&semt=sph"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <div className="ml-4 ">
+                                <p className="text-gray-100 text-2xl">benWoods</p>
+                                <p className="text-lg text-gray-400">Ben Woods</p>
+                            </div>
+                        </div>
+                        <p className="text-sky-400 font-semibold text-xl">Follow</p>
+                    </div>
+                    <div className="flex justify-around items-center">
+                        <div className="flex justify-center items-center mt-4">
+                            <div className="avatar">
+                                <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                    <img
+                                        src="https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611710.jpg?size=626&ext=jpg&ga=GA1.1.448863966.1693684736&semt=sph"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <div className="ml-4 ">
+                                <p className="text-gray-100 text-2xl">marquess_shapiro45</p>
+                                <p className="text-lg text-gray-400">Marquess Shapiro</p>
+                            </div>
+                        </div>
+                        <p className="text-sky-400 font-semibold text-xl">Switch</p>
+                    </div>
+                    <div className="flex justify-around items-center">
+                        <div className="flex justify-center items-center mt-4">
+                            <div className="avatar">
+                                <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                    <img
+                                        src="https://img.freepik.com/premium-psd/3d-render-cartoon-avatar-isolated_570939-100.jpg?size=626&ext=jpg&ga=GA1.1.448863966.1693684736&semt=sph"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <div className="ml-4 ">
+                                <p className="text-gray-100 text-2xl">alissa_blair88</p>
+                                <p className="text-lg text-gray-400">Alissa Blair</p>
+                            </div>
+                        </div>
+                        <p className="text-sky-400 font-semibold text-xl">Switch</p>
+                    </div>
+                    <div className="flex justify-around items-center">
+                        <div className="flex justify-center items-center mt-4">
+                            <div className="avatar">
+                                <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                    <img
+                                        src="https://img.freepik.com/free-psd/3d-illustration-person_23-2149436192.jpg?size=626&ext=jpg&ga=GA1.1.448863966.1693684736&semt=sph"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <div className="ml-4 ">
+                                <p className="text-gray-100 text-2xl">alissa_blair88</p>
+                                <p className="text-lg text-gray-400">Alissa Blair</p>
+                            </div>
+                        </div>
+                        <p className="text-sky-400 font-semibold text-xl">Switch</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RightNav;
